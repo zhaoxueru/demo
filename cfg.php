a16a5351a9b31bb8f2c218b77cbadfcb2ca4703f
Jerry <27206617@qq.com>
2018-01-24 14:13:40 +0800

    simple test script

---
+++ cfg.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Examples
+ * Remember to change to fit your own
+ */
+
+\ext\crypt::$keygen = '\demo\keygen';
+\ext\crypt::$ssl_cnf = '/extras/ssl/openssl.cnf';
---
+++ test.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Crypt Extension TEST Script
+ *
+ * Author 秋水之冰 <27206617@qq.com>
+ *
+ * Copyright 2018 秋水之冰
+ *
+ * This file is part of NervSys.
+ *
+ * NervSys is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * NervSys is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with NervSys. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace demo;
+
+use \ext\crypt;
+
+class test
+{
+    public static $key = [
+        'crypt' => []
+    ];
+
+    private static function chk_string(string $name, array $data): void
+    {
+        echo $name . ': ' . ($data[0] === $data[1] ? 'PASSED!' : 'Failed! ' . $data[0] . ' !== ' . $data[1]);
+        echo PHP_EOL;
+    }
+
+    private static function chk_bool(string $name, bool $data): void
+    {
+        echo $name . ': ' . ($data ? 'PASSED!' : 'Failed!');
+        echo PHP_EOL;
+    }
+
+    public static function crypt(): void
+    {
+        echo 'Crypt Test Starts:';
+        echo PHP_EOL;
+        echo 'Make sure to set the right path of "openssl.cnf" in "cfg.php"';
+        echo PHP_EOL;
+        echo 'You can provide your own "keygen" class script in "cfg.php"';
+        echo PHP_EOL;
+        echo PHP_EOL;
+
+        $string = (string)mt_rand();
+
+        $aes_key = forward_static_call([crypt::$keygen, 'create']);
+
+        $enc = crypt::encrypt($string, $aes_key);
+        $dec = crypt::decrypt($enc, $aes_key);
+        self::chk_string('encrypt/decrypt', [$string, $dec]);
+
+
+        $rsa_key = crypt::rsa_keys();
+
+        $enc = crypt::rsa_encrypt($string, $rsa_key['public']);
+        $dec = crypt::rsa_decrypt($enc, $rsa_key['private']);
+        self::chk_string('rsa_encrypt(pub)/rsa_decrypt(pri)', [$string, $dec]);
+
+        $enc = crypt::rsa_encrypt($string, $rsa_key['private']);
+        $dec = crypt::rsa_decrypt($enc, $rsa_key['public']);
+        self::chk_string('rsa_encrypt(pri)/rsa_decrypt(pub)', [$string, $dec]);
+
+
+        $enc = crypt::sign($string);
+        $dec = crypt::verify($enc);
+        self::chk_string('sign/verify', [$string, $dec]);
+
+        $enc = crypt::sign($string, $rsa_key['public']);
+        $dec = crypt::verify($enc, $rsa_key['private']);
+        self::chk_string('sign(pub)/verify(pri)', [$string, $dec]);
+
+        $enc = crypt::sign($string, $rsa_key['private']);
+        $dec = crypt::verify($enc, $rsa_key['public']);
+        self::chk_string('sign(pri)/verify(pub)', [$string, $dec]);
+
+        $hash = crypt::hash_pwd($string, $aes_key);
+        $pwd_chk = crypt::check_pwd($string, $aes_key, $hash);
+        self::chk_bool('hash_pwd/check_pwd', $pwd_chk);
+    }
+}
